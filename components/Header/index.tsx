672a1abf26ec16d518650c9f6c4a8caa31081e2e
krishna10k <krishnaprasadawala@gmail.com>
2024-04-07 13:39:56 +0530

    changes

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,53 +1,122 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState<boolean>(false);
-  useEffect(() => {
+  const [isSmall, setIsSmall] = useState<boolean>();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
+  useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = document.documentElement.scrollTop;
       setIsScrolled(scrollPosition > 0);
-    }
-    window.addEventListener("scroll", handleScroll)
+    };
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener("scroll", handleScroll)
-    }
-  })
-
-      const scrollToWork = () => {
-      window.scrollTo({
-        top: 1000,
-        behavior: "smooth"
-      });
+      window.removeEventListener("scroll", handleScroll);
     };
+  });
 
+  const scrollToWork = () => {
+    window.scrollTo({
+      top: 1000,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const thresholdHeight = 830;
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= thresholdHeight) {
+        setIsSmall(true);
+        setIsOpen(false);
+      } else {
+        setIsSmall(false);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <header className={`flex items-center w-screen h-[10vh] fixed top-0 left-0 z-[1000] overflow-hidden ${isScrolled && `backdrop-blur-sm bg-black/50  border-b border-gray-900`}`}>
-      <Logo />
-      <div className="flex justify-center items-center gap-2 md:gap-[30px] pl-5 md:pl-16">
-        <Link className=" text-gray-400 text-sm font-bold hover:text-[15px] transition-all ease-in-out " href={"#services"} >Our Services</Link>
-        <p className=" text-gray-400 text-sm font-bold hover:text-[15px] transition-all ease-in-out cursor-pointer" onClick={scrollToWork}  >Our Work</p>
-        <Link className=" text-gray-400 text-sm font-bold hover:text-[15px] transition-all ease-in-out" href={"#testimonials"} >Testimonials</Link>
+    <header
+      className={`flex flex-col items-center w-screen px-5 py-[2vh] lg:px-[20vw] h-[10vh] fixed top-0 left-0 z-[1000] overflow-hidden ${
+        (isScrolled || isOpen) && `backdrop-blur-sm bg-black/50  border-b border-gray-900`
+      } ${isOpen && `h-screen`} ` }
+    >
+      <div className="flex justify-between items-center w-full">
+        <Logo />
+        {isSmall ? (
+          <button className="text-white scale-150" onClick={() => setIsOpen(!isOpen)}><GiHamburgerMenu /></button>
+        ) : (
+          <div className="flex justify-center items-center gap-2 md:gap-[30px] pl-5 md:pl-16">
+            <Link
+              className=" text-gray-400 text-sm font-bold hover:text-[15px] transition-all ease-in-out "
+              href={"#services"}
+            >
+              Our Services
+            </Link>
+            <p
+              className=" text-gray-400 text-sm font-bold hover:text-[15px] transition-all ease-in-out cursor-pointer"
+              onClick={scrollToWork}
+            >
+              Our Work
+            </p>
+            <Link
+              className=" text-gray-400 text-sm font-bold hover:text-[15px] transition-all ease-in-out"
+              href={"#testimonials"}
+            >
+              Testimonials
+            </Link>
+          </div>
+        )}
       </div>
+      {(isSmall && isOpen) && (
+        <div className={`flex h-full flex-col justify-center items-center gap-10 md:gap-[30px]  md:pl-16`}>
+          <Link
+            className=" text-gray-400 text-sm font-bold hover:text-[15px] transition-all ease-in-out "
+            href={"#services"}
+          >
+            Our Services
+          </Link>
+          <p
+            className=" text-gray-400 text-sm font-bold hover:text-[15px] transition-all ease-in-out cursor-pointer"
+            onClick={scrollToWork}
+          >
+            Our Work
+          </p>
+          <Link
+            className=" text-gray-400 text-sm font-bold hover:text-[15px] transition-all ease-in-out"
+            href={"#testimonials"}
+          >
+            Testimonials
+          </Link>
+        </div>
+      )}
     </header>
   );
 }
 
-
 const Logo = () => {
   const router = useRouter();
   return (
-    <div onClick={() => router.push("/")} className="flex items-center gap-3 opacity-90 cursor-pointer pl-5 md:pl-[16%] hover:opacity-100 transition-all ease-in-out">
+    <div
+      onClick={() => router.push("/")}
+      className="flex items-center gap-3 opacity-90 cursor-pointer hover:opacity-100 transition-all ease-in-out"
+    >
       <Image src={"/Logo/Logo.png"} alt="Logo" width={35} height={40} />
-    <h1 className=" text-white font-[750] text-xl pb-1 hidden md:block">Avisa Systems</h1>
+      <h1 className=" text-white font-[750] text-xl pb-1">Avisa Systems</h1>
     </div>
   );
 };
