f245efb204445d7e46b81f3b03176e69c178598b
krishna10k <krishnaprasadawala@gmail.com>
2024-04-08 19:13:17 +0530

    changes

--- components/ui/hero-parallax.tsx
+++ components/ui/hero-parallax.tsx
@@ -57,7 +57,7 @@ export const HeroParallax = ({
   return (
     <div
       ref={ref}
-      className=" h-[1500px] md:h-[2200px] overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      className=" h-[1550px] md:h-[2200px] overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
     >
       <Header />
       <motion.div
