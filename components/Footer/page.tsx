fbe5a1fccf5ef3ad226f9fbb21fd266273e853b9
krishna10k <krishnaprasadawala@gmail.com>
2024-04-08 19:28:43 +0530

    changes

--- components/Footer/page.tsx
+++ components/Footer/page.tsx
@@ -39,7 +39,7 @@ const Footer = () => {
   };
 
   return (
-    <div className="grid grid-cols-2 grid-rows-2 justify-center items-center backdrop-blur-sm bg-[#13151B]/40 dark: text-green-50">
+    <div className="grid grid-cols-2 grid-rows-2 justify-center items-center backdrop-blur-sm bg-[#13151B]/40 dark: text-white">
       <div className=" col-span-2 md:col-span-1">
         <div className="flex flex-col gap-16 justify-center items-start p-5 md:pl-24">
           <h1 className="text-6xl">Crafting Tomorrow's Solutions Today.</h1>
