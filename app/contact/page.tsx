2e904c5faf8bd28e992554f0223da6ce53e0333c
krishna10k <krishnaprasadawala@gmail.com>
2024-04-11 16:53:40 +0530

    changes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,7 +10,10 @@ const Contact = () => {
     <div className="dark">
       <div className="flex flex-col lg:flex-row py-24">
         <Header />
-        <div className=" flex-1 lg:pt-10">
+        <div className=" relative flex-1 lg:pt-10">
+          <div className=" absolute lg:hidden w-[100%] h-full z-50 ">
+
+          </div>
           <Globe />
         </div>
         <div className=" flex-1 overflow-hidden relative flex justify-center items-center">
