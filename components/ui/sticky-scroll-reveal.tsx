ad15a529ca9ecd5827fa149d9fb69ca2a95c68d6
krishna10k <krishnaprasadawala@gmail.com>
2024-04-15 20:07:56 +0530

    changes

--- components/ui/sticky-scroll-reveal.tsx
+++ components/ui/sticky-scroll-reveal.tsx
@@ -96,7 +96,7 @@ export const StickyScroll = ({
         <div className="div relative flex items-start px-4">
           <div className="max-w-2xl">
             {content.map((item, index) => (
-              <div key={item.title + index} className="my-20">
+              <div key={item.title + index} className="my-20 pb-10">
                 <motion.h2
                   initial={{
                     opacity: 0,
